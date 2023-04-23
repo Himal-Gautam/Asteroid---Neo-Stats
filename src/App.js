8fa70afc8bed53468d5a565cad209e7d01a36204
Himal Gautam <himalpgautam@gmail.com>
2023-04-23 23:10:41 +0530

    added required comments

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ ChartJS.register(
 );
 
 export function App() {
+  //declaration of require states
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [data, setData] = useState({});
@@ -38,9 +39,11 @@ export function App() {
   const [stats, setStats] = useState("");
   const [show, setShow] = useState(false);
 
+  // handling opening & closing of modal
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  // formating date as per requirement
   const formatDate = (date) => {
     const year = date.getFullYear();
     let month = date.getMonth() + 1;
@@ -57,15 +60,18 @@ export function App() {
     return `${year}-${month}-${day}`;
   };
 
+  // fetching data from api on button click
   const fetchData = async () => {
+    // getting formated date
     const start = formatDate(startDate);
     const end = formatDate(endDate);
 
+    // declaring constants for validation purpose
     const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
     const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
     const today = formatDate(new Date());
 
+    // validating dates
     if (start === end) {
       setError("Start date and end date should not be the same");
       return;
@@ -83,22 +89,31 @@ export function App() {
     setLoading(true);
     setError("");
 
+
     try {
+      // calling api
       const response = await axios.get(
         `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start}&end_date=${end}&api_key=XCae61RcTawn0ec3bwICL2Ri02sCvpcResQ7hmT3`
       );
 
+      // generating dates for labels for chart
       const dates = Object.keys(response.data.near_earth_objects);
+
       const asteroidCount = [];
       let fastestAsteroid = {};
       let closestAsteroid = {};
       let avgSizeOfAsteroid = 0;
       // let totalAsteroids = 0;
 
+
       dates.forEach((date) => {
+
+        // getting the count of asteroids on a date
         let count = response.data.near_earth_objects[date].length;
         asteroidCount.push(count);
         // totalAsteroids += count;
+
+        // getting the fastest & closest asteroid  and avgsize data 
         response.data.near_earth_objects[date].forEach((asteroid) => {
           if (
             !fastestAsteroid.speed ||
@@ -136,6 +151,7 @@ export function App() {
         });
       });
 
+      // limitng the decimal
       avgSizeOfAsteroid = avgSizeOfAsteroid.toFixed(2);
 
       setStats({
@@ -143,11 +159,6 @@ export function App() {
         closestAsteroid,
         avgSizeOfAsteroid,
       });
-      // setStats({
-      //   fastestAsteroid: fastestAsteroid,
-      //   closestAsteroid: closestAsteroid,
-      //   avgSizeOfAsteroid: (avgSizeOfAsteroid / totalAsteroids).toFixed(2),
-      // });
 
       setData({
         labels: dates,
@@ -162,18 +173,6 @@ export function App() {
         ],
       });
 
-      console.log({
-        labels: dates,
-        datasets: [
-          {
-            label: "Number of asteroids",
-            data: asteroidCount,
-            fill: false,
-            borderColor: "rgb(255, 99, 132)",
-            backgroundColor: "rgba(255, 99, 132, 0.5)",
-          },
-        ],
-      });
     } catch (error) {
       console.log(error);
     }
@@ -181,6 +180,7 @@ export function App() {
     setLoading(false);
   };
 
+  // options prop for chart component
   const options = {
     responsive: true,
     plugins: {
@@ -200,6 +200,7 @@ export function App() {
   return (
     <>
       <Container fluid>
+        {/* header with navbar */}
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
@@ -216,6 +217,8 @@ export function App() {
             </Container>
           </Navbar>
         </header>
+
+        {/* row for form */}
         <Row
           style={{
             margin: "auto",
@@ -273,8 +276,10 @@ export function App() {
           </Col>
         </Row>
 
+        {/* showing table and stats only if data is available */}
         {Object.keys(data).length > 0 && (
           <>
+            {/* row for showing chart */}
             <Row
               className="my-5"
               style={{
@@ -299,6 +304,8 @@ export function App() {
                 />
               </Col>
             </Row>
+
+            {/* row for showing stats */}
             <Row
               style={{
                 margin: "auto",
@@ -355,6 +362,8 @@ export function App() {
           </>
         )}
       </Container>
+
+      {/* modal for showing date constraints */}
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Date Constraints</Modal.Title>
